1ab2b2745219904e682c201798914cacc5caca86
Wef2 <qhrud5725@hanmail.net>
2016-05-23 17:30:02 +0900

    Add Map Fragment

--- HomeSharing/app/src/main/java/mcl/jejunu/ac/homesharing/activity/HomeInformationActivity.java
+++ HomeSharing/app/src/main/java/mcl/jejunu/ac/homesharing/activity/HomeInformationActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mcl.jejunu.ac.homesharing.R;
 
-public class HomeInformationActivity extends AppCompatActivity {
+public class HomeInformationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private int homeId;
+    private GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,19 @@ public class HomeInformationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mGoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
