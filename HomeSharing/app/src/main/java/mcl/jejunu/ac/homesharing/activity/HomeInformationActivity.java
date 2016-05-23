b4589fc28f31da4c80f66e0133a4339e1792387d
Wef2 <qhrud5725@hanmail.net>
2016-05-23 18:40:25 +0900

    Add FloatingActionButton, Add ReservationActivity(Empty)

--- HomeSharing/app/src/main/java/mcl/jejunu/ac/homesharing/activity/HomeInformationActivity.java
+++ HomeSharing/app/src/main/java/mcl/jejunu/ac/homesharing/activity/HomeInformationActivity.java
@@ -1,8 +1,12 @@
 package mcl.jejunu.ac.homesharing.activity;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -42,15 +46,31 @@ public class HomeInformationActivity extends AppCompatActivity implements OnMapR
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.reservation_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeInformationActivity.this, ReservationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mGoogleMap.setMyLocationEnabled(true);
+        } else {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mGoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(50));
+        LatLng jeju = new LatLng(33.499234, 126.530714);
+        mGoogleMap.addMarker(new MarkerOptions().position(jeju).title("제주시"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jeju, 10));
+
     }
+
 }
---
+++ HomeSharing/app/src/main/java/mcl/jejunu/ac/homesharing/activity/ReservationActivity.java
@@ -0,0 +1,15 @@
+package mcl.jejunu.ac.homesharing.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mcl.jejunu.ac.homesharing.R;
+
+public class ReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+}
